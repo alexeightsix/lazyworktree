0d0c854edc963d3e8b60550922f8bc604b161432
Alexander Latour <alexlatour@gmail.com>
2024-03-15 11:51:31 -0400

    fixes

--- src/CLIApplication.php
+++ src/CLIApplication.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace App;
 
+
 use App\Actions\Add;
 use App\Actions\Change;
 use App\Actions\Delete;
-use App\Actions\Quit;
 use App\Actions\Link;
+use App\Actions\Quit;
 use App\Actions\Setup;
+
+use App\ApiOptions;
 
 use Exception;
 
@@ -19,14 +22,12 @@ use function laravel\Prompts\select;
 
 class CLIApplication
 {
+
   private array $options;
   private array $menuItems = [];
 
   private const OPT_INIT = 'init';
   private const OPT_SWITCH = 'switch';
-
-  private const API_ACTION_SWITCH = "switch";
-  private const API_ACTION_LIST = "list";
 
   public function __construct(array $options)
   {
@@ -43,25 +44,44 @@ class CLIApplication
     $this->menuItems[$action] = $action::MENU_NAME; // @phpstan-ignore-line
   }
 
+  private function isValidApiAction(string $api_action): bool
+  {
+    return in_array(
+      $api_action,
+      [
+        ApiOptions::switch->name,
+        ApiOptions::list->name,
+        ApiOptions::health->name,
+      ]
+    );
+  }
+
   private function handleApi(): void
   {
-    $api_action = $this->options[2] ?? null;
+    $api_action = (string) $this->options[2] ?? null;
 
-    if (!in_array($api_action, [self::API_ACTION_SWITCH, self::API_ACTION_LIST])) {
+    if (!$this->isValidApiAction($api_action)) {
       error('Invalid API action.');
       exit(1);
     }
 
-    $git_root = Helpers::findGitFolder();
-
-    $worktrees = GitService::getWorktrees(git_path: $git_root);
-
-    if ($api_action === self::API_ACTION_LIST) {
-      echo $worktrees->getJson();
-      exit(0);
+    try {
+      $git_root = Helpers::findGitFolder();
+      $worktrees = GitService::getWorktrees(git_path: $git_root);
+    } catch (Exception) {
+      $invalid = true;
     }
 
-    if ($api_action === self::API_ACTION_SWITCH) {
+    if ($api_action === ApiOptions::health->name) {
+      if (isset($invalid)) {
+        exit(1);
+      } else {
+        exit(0);
+      }
+    } elseif ($api_action === ApiOptions::list->name) {
+      echo $worktrees->getJson();
+      exit(0);
+    } elseif ($api_action === ApiOptions::health->name) {
       $option = $this->options[3] ?? null;
 
       if ($option === null) {
@@ -91,14 +111,14 @@ class CLIApplication
 
     $init = $this->hasOption(self::OPT_INIT);
 
-    if (!$init && !isset($initalized)) {
-      error('No lazywt.json file found. Please run `lazywt init` in the root of your project.');
-      exit(1);
-    }
-
     if ($this->hasOption("api")) {
       $this->handleApi();
       exit(0);
+    }
+
+    if (!$init && !isset($initalized)) {
+      error('No lazywt.json file found. Please run `lazywt init` in the root of your project.');
+      exit(1);
     }
 
     foreach (["add", "switch", "delete"] as $option) {
